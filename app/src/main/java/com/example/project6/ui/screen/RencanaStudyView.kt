fadb20119fc806bef607cc96c2d75f6639439ec4
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-11-22 15:27:04 +0700

    menambahkan image pada topbar

--- app/src/main/java/com/example/project6/ui/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/project6/ui/screen/RencanaStudyView.kt
@@ -1,10 +1,26 @@
 package com.example.project6.ui.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.project6.R
 import com.example.project6.model.Mahasiswa
 import com.example.project6.model.RencanaStudy
 
@@ -20,5 +36,24 @@ fun RencanaStudyView(
     var checked by remember { mutableStateOf(false) }
     var pilihanKelas by remember {
         mutableStateOf("")
+    }
+    var listData: MutableList<String> = mutableListOf(chosenDropdown, pilihanKelas)
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .background(color = colorResource(id = R.color.primary))) {
+
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp), verticalAlignment = Alignment.CenterVertically) {
+            Image(
+                painter = painterResource(id = R.drawable.img),
+                contentDescription = "",
+                modifier = Modifier
+                    .clip(shape = CircleShape)
+                    .size(50.dp)
+            )
+
+        }
+
     }
 }
