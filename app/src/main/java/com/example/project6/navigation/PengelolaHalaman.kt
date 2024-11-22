38808d8aa488df2a83997f68189dc9683618d6c3
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-11-22 16:03:39 +0700

    menambahkan variabel mahasiswaUiState dan membuat navhost dengan hal pertama splash

--- app/src/main/java/com/example/project6/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/project6/navigation/PengelolaHalaman.kt
@@ -1,9 +1,12 @@
 package com.example.project6.navigation
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.example.project6.ui.viewmodel.MahasiswaViewModel
 import com.example.project6.ui.viewmodel.RencanaStudyViewModel
@@ -21,5 +24,14 @@ fun MahasiswaApp(
     krsViewModel: RencanaStudyViewModel = viewModel(),
     navController: NavHostController = rememberNavController()
 ){
+    val mahasiswaUiState = mahasiswaViewModel.mahasiswaUiState.collectAsState().value
+
+    NavHost(
+        navController = navController,
+        startDestination = Halaman.Splash.name,
+        modifier = Modifier.padding()
+    ){
+
+    }
 
 }
